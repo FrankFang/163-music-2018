3ff5891ad6ed4d94dbafda0390af6d9924c6ff42
Frank Fang <FrankFang1990@gmail.com>
2018-02-16 22:09:02 +0800

    更新数据后刷新视图

--- src/js/song-form.js
+++ src/js/song-form.js
@@ -50,6 +50,16 @@
     data: {
       name: '', singer: '', url: '', id: ''
     },
+    update(data){
+      var song = AV.Object.createWithoutData('Song', this.data.id)
+      song.set('name', data.name)
+      song.set('singer', data.singer)
+      song.set('url', data.url)
+      return song.save().then((response)=>{
+        Object.assign(this.data, data)
+        return response
+      })
+    },
     create(data){
       var Song = AV.Object.extend('Song');
       var song = new Song();
@@ -86,22 +96,41 @@
         this.view.render(this.model.data)
       })
     },
+    create(){
+      let needs = 'name singer url'.split(' ')
+      let data = {}
+      needs.map((string)=>{
+        data[string] = this.view.$el.find(`[name="${string}"]`).val()
+      })
+      this.model.create(data)
+        .then(()=>{
+          this.view.reset()
+          //this.model.data === 'ADDR 108'
+          let string = JSON.stringify(this.model.data)
+          let object = JSON.parse(string)
+          window.eventHub.emit('create', object)
+        })
+    },
+    update(){
+      let needs = 'name singer url'.split(' ')
+      let data = {}
+      needs.map((string)=>{
+        data[string] = this.view.$el.find(`[name="${string}"]`).val()
+      })
+      this.model.update(data)
+        .then(()=>{
+          window.eventHub.emit('update', JSON.parse(JSON.stringify(this.model.data)))
+        })
+    },
     bindEvents(){
       this.view.$el.on('submit', 'form', (e)=>{
         e.preventDefault()
-        let needs = 'name singer url'.split(' ')
-        let data = {}
-        needs.map((string)=>{
-          data[string] = this.view.$el.find(`[name="${string}"]`).val()
-        })
-        this.model.create(data)
-          .then(()=>{
-            this.view.reset()
-            //this.model.data === 'ADDR 108'
-            let string = JSON.stringify(this.model.data)
-            let object = JSON.parse(string)
-            window.eventHub.emit('create', object)
-          })
+
+        if(this.model.data.id){
+          this.update()
+        }else{
+          this.create()
+        }
       })
     }
   }
--- src/js/song-list.js
+++ src/js/song-list.js
@@ -78,6 +78,15 @@
       window.eventHub.on('new',()=>{
         this.view.clearActive()
       })
+      window.eventHub.on('update', (song)=>{
+        let songs = this.model.data.songs
+        for(let i=0; i<songs.length; i++){
+          if(songs[i].id === song.id){
+            Object.assign(songs[i],song)
+          }
+        }
+        this.view.render(this.model.data)
+      })
     }
   }
 
