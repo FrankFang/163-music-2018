536271198a3dd3a4d8f8ce8c5acb34257f5f9911
Frank Fang <FrankFang1990@gmail.com>
2018-02-16 21:05:20 +0800

    获取歌曲列表

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -24,6 +24,15 @@
   let model = {
     data: {
       songs: [ ]
+    },
+    find(){
+      var query = new AV.Query('Song');
+      return query.find().then((songs)=>{
+        this.data.songs = songs.map((song)=>{
+          return {id: song.id, ...song.attributes}
+        })
+        return songs
+      })
     }
   }
   let controller = {
@@ -31,6 +40,18 @@
       this.view = view
       this.model = model
       this.view.render(this.model.data)
+      this.bindEvents()
+      this.getAllSongs()
+    },
+    getAllSongs(){
+      this.model.find().then(()=>{
+        this.view.render(this.model.data)
+      })
+    },
+    bindEvents(){
+      
+    },
+    bindEventHub(){
       window.eventHub.on('upload', ()=>{
         this.view.clearActive()
       })
