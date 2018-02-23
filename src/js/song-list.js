fc5e24916c0275b492369a476cba070287b78c8b
Frank Fang <FrankFang1990@gmail.com>
2018-02-23 20:45:05 +0800

    update

--- src/js/song-list.js
+++ src/js/song-list.js
@@ -8,19 +8,16 @@
     render(data){
       let $el = $(this.el)
       $el.html(this.template)
-      let {songs} = data
-      let liList = songs.map((song)=> 
-        $('<li></li>').text(song.name).attr('data-song-id', song.id)
-      )
+      let {songs, selectedSongId} = data
+      let liList = songs.map((song)=> {
+        let $li = $('<li></li>').text(song.name).attr('data-song-id', song.id)
+        if(song.id === selectedSongId){ $li.addClass('active') }
+        return $li
+      })
       $el.find('ul').empty()
       liList.map((domLi)=>{
         $el.find('ul').append(domLi)
       })
-    },
-    activeItem(li){
-      let $li = $(li)
-      $li.addClass('active')
-        .siblings('.active').removeClass('active')
     },
     clearActive(){
       $(this.el).find('.active').removeClass('active')
@@ -28,7 +25,8 @@
   }
   let model = {
     data: {
-      songs: [ ]
+      songs: [ ],
+      selectSongId: undefined,
     },
     find(){
       var query = new AV.Query('Song');
@@ -56,8 +54,11 @@
     },
     bindEvents(){
       $(this.view.el).on('click', 'li', (e)=>{
-        this.view.activeItem(e.currentTarget)
         let songId = e.currentTarget.getAttribute('data-song-id')
+
+        this.model.data.selectedSongId = songId
+        this.view.render(this.model.data)
+
         let data
         let songs =this.model.data.songs
         for(let i = 0; i<songs.length; i++){
