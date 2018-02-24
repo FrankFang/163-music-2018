2c5312b357822b2960f15d4709604942c7286663
Frank Fang <FrankFang1990@gmail.com>
2018-02-24 13:21:05 +0800

    添加播放暂停功能

--- src/js/index/page-1-2.js
+++ src/js/index/page-1-2.js
@@ -10,7 +10,7 @@
             </svg>
             {{song.singer}}
           </p>
-          <a class="playButton" href="#">
+          <a class="playButton" href="./song.html?id={{song.id}}">
             <svg class="icon icon-play">
               <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-play"></use>
             </svg>
@@ -26,6 +26,7 @@
         let $li = $(this.template
           .replace('{{song.name}}', song.name)
           .replace('{{song.singer}}', song.singer)
+          .replace('{{song.id}}', song.id)
         )
         this.$el.find('ol.list').append($li)
       })
